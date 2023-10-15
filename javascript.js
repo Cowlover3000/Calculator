d50646c73335621eeab54fb370d66a2e61d13bb2
Cowlover3000 <niels851@hotmail.com>
2023-10-15 22:28:04 +0200

    getting stuf done

--- javascript.js
+++ javascript.js
@@ -1,19 +1,61 @@
+let displayValue = 0;
+let keys = document.querySelectorAll(".key");
+
 function add(num1, num2) {
-    return num1 + num2;
+    console.log(num1 + num2); 
 }
 
 function minus(num1, num2){
-    return num1 - num2;
+    console.log(num1 - num2);
 }
 
 function division(num1, num2){
     if (num2 === 0) {
         throw new Error("You can't divide by zero dummy!");
     } else {
-        return Math.round(num1 / num2);
+        console.log(num1 / num2);
     }
 }
 
 function multiply(num1, num2){
-    return num1 * num2;
+    console.log(num1 * num2);
 }
+
+function operate(num1, num2, operator){
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            minus(num1, num2);
+            break;
+        case "/":
+            try {
+                division(num1, num2)
+            } catch (error) {
+                console.error(error);
+            }
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+function populate(text){
+    const screen = document.querySelector("#screen");
+    screen.textContent += text;
+    displayValue = parseInt(screen.textContent);
+}
+
+keys.forEach((key) => {
+    key.addEventListener('click', () => {
+        if (key.textContent.match(/[0-9]/)) {
+            populate(key.textContent);
+        } else if (key.textContent.match(/[+\-*\/]/)) {
+            console.log("ok");
+        }
+    })
+})
